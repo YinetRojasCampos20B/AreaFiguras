81fab54ae32dbca6340f0ad0bf8a41700cf776e0
YinetRojasCampos20B <rojascamposyinet1101@gmail.com>
2026-09-22 12:19:28 -0500

    AJustando la fórmula del perímetro para triángulos regulares y creando un método auxiliar en la clase principal para solicitar los lados del triángulo.

--- Main.java
+++ Main.java
@@ -1,7 +1,77 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+
+        imprimirMenuInicio();
+
+        Scanner entrada = new Scanner(System.in);
+
+        int opcion = entrada.nextInt();
+
+        switch (opcion) {
+            case 1:
+                /* CÍRCULO */
+                System.out.println("Ingrese el radio del círculo en centímetros: ");
+
+                break;
+            case 2:
+                /* CUADRADO */
+                System.out.println("Ingrese la longitud del lado del cuadrado en centímetros: ");
+
+                break;
+            case 3:
+
+                /** TRIÁNGULO */
+
+                System.out.println("Ingrese el valor de la base del triángulo en centímetros: ");
+                double baseTriangulo = entrada.nextDouble();
+
+                System.out.println("Ingrese el valor de la altura del triángulo en centímetros: ");
+                double alturaTriangulo = entrada.nextDouble();
+
+                // Instanciación del objeto Triángulo
+
+                Triangulo trianguloUsuario = new Triangulo(baseTriangulo, alturaTriangulo, lado1, lado2, lado3);
+
+                break;
         
-    
-}
+            default:
+                break;
+        }
+
+    }
+
+    // Método auxiliar para imprimir el menú de inicio
+
+    public static void imprimirMenuInicio() {
+        System.out.println("Bienvenido a la calculadora de áreas y perímetros");
+        System.out.println("Seleccione una figura para comenzar: ");
+
+        System.out.println("1) Círculo");
+        System.out.println("2) Cuadrado");
+        System.out.println("3) Triángulo no rectángulo");
+    }
+
+    // Método para solicitar los valores de los lados del triángulo al usuario
+
+    public static void solicitarLadosTriangulo(Scanner entrada) {
+
+        // Solicitar el ingreso de los lados del triángulo
+                
+        System.out.println("Ingrese el valor del lado 1");
+
+        double lado1 = entrada.nextDouble();
+        
+
+    }
+
+
+    // Método para 
+
+    public static areaPerimetroCirculo() {
+
+
+    }
 
 }
--- Triangulo.java
+++ Triangulo.java
@@ -12,6 +12,16 @@ public class Triangulo {
     private double lado2;
     private double lado3;
 
+    // Método constructor para crear objetos de la clase concreta Triángulo
+
+    public Triangulo(double base, double altura, double lado1, double lado2, double lado3) {
+        this.base = base;
+        this.altura = altura;
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.lado3 = lado3;
+    }
+
     // Implementación en Triángulo de los métodos calcularArea y calcularPerimetro de la clase abstracta Figura
 
     public double calcularArea() {
